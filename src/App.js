a723cadd2c5f8337d63fa2e6d9b1372f105795cf
Max <max.fsmy@gmail.com>
2021-09-20 18:41:10 -0500

    finished background figures

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <section className="ilustration">
+        <div className="background-figure"></div>
+        <div className="ilustration__cellphone"></div>
+      </section>
+      <section className="app-description borde">
+        <div className="background-figure background-figure--inverted-grey"></div>
+      </section>
+    </main>
   );
 }
 
