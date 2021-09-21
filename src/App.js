51fc06565c7fff0a3ee8e93fdae3722d33d5e317
Max <max.fsmy@gmail.com>
2021-09-20 21:55:04 -0500

    finished text side

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
         <div className="background-figure"></div>
         <div className="ilustration__cellphone"></div>
       </section>
-      <section className="app-description borde">
+      <section className="app-description">
+        <div className="app-description__text">
+          <h1 className="app-description__title">Simple booking</h1>
+          <p className="app-description__description">
+            Stay in touch with our dog walkers through the chat interface. This makes it easy to 
+            discuss arrangements and make bookings. Once the walk has been completed you can rate 
+            your walker and book again all through the chat.
+          </p>
+        </div>
         <div className="background-figure background-figure--inverted-grey"></div>
       </section>
     </main>
